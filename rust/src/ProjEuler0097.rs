dca755c31827e5de174de8049a61c5ad9776915a
rahulnene <rahul.nene@live.com>
2023-02-15 21:19:44 +0530

    Added problem 97

---
+++ rust/src/ProjEuler0097.rs
@@ -0,0 +1,10 @@
+fn main() {
+    dbg!((mod_pow(2, 7830457) * 28433 + 1) % DIGITS);
+}
+const DIGITS: u64 = 10_u64.pow(10);
+
+fn mod_pow(a: u64, b: u64) -> u64 {
+    (0..b)
+        .fold(1, |res, _| (res * a)% DIGITS)
+        .rem_euclid(DIGITS)
+}
