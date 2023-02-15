2a770253945478e30c520424a84a7a267f134c24
rahulnene <rahul.nene@live.com>
2023-02-15 20:26:25 +0530

    Added problem 48

--- rust/src/ProjEuler0048.rs
+++ rust/src/ProjEuler0048.rs
@@ -7,6 +7,6 @@ const DIGITS: u64 = 10_u64.pow(10);
 
 fn mod_pow(a: u64, b: u64) -> u64 {
     (0..b)
-        .fold(1, |res,_| (res * a).rem_euclid(DIGITS))
+        .fold(1, |res, _| (res * a).rem_euclid(DIGITS))
         .rem_euclid(DIGITS)
 }
