c69a2953fcee4a0beeade5e860d2eddf661dfa71
rahulnene <rahul.nene@live.com>
2023-02-15 19:50:58 +0530

    test

--- rust/src/ProjEuler0015.rs
+++ rust/src/ProjEuler0015.rs
@@ -1,5 +1,5 @@
 fn main() {
     let a = (21..=40).fold(1_u128, |res, n| res * n);
-    let b = (2..=20).fold(1_u128, |res,n| res*n);
-    dbg!(a/b);
+    let b = (2..=20).fold(1_u128, |res, n| res * n);
+    dbg!(a / b);
 }
