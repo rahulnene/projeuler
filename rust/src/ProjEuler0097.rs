264c8d435386b940db897891fc10b031f4a1749a
rahulnene <rahul.nene@live.com>
2023-02-15 21:22:04 +0530

    formatting

--- rust/src/ProjEuler0097.rs
+++ rust/src/ProjEuler0097.rs
@@ -1,10 +1,9 @@
-fn main() {
-    dbg!((mod_pow(2, 7830457) * 28433 + 1) % DIGITS);
-}
 const DIGITS: u64 = 10_u64.pow(10);
-
 fn mod_pow(a: u64, b: u64) -> u64 {
     (0..b)
-        .fold(1, |res, _| (res * a)% DIGITS)
+        .fold(1, |res, _| (res * a) % DIGITS)
         .rem_euclid(DIGITS)
+}
+fn main() {
+    println!("{}", (mod_pow(2, 7830457) * 28433 + 1) % DIGITS);
 }
