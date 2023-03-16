4128726e39208296b0cd6861915486127d9f0d47
rahulnene <rahul.nene@live.com>
2023-03-17 00:12:37 +0530

    .

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,21 +1,3 @@
-use std::fs;
 fn main() {
-    let pyramid_str = fs::read_to_string("p067_triangle.txt").unwrap();
-    let mut pyramid: Vec<Vec<u32>> = Vec::new();
-    for line in pyramid_str.lines() {
-        pyramid.push(
-            line.split_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect(),
-        );
-    }
-    while pyramid.len() > 1 {
-        let last_row = pyramid.pop().unwrap();
-        let mut bottom_row = pyramid.pop().unwrap();
-        for index in 0..bottom_row.len() {
-            bottom_row[index] += last_row[index].max(last_row[index+1]);
-        }
-        pyramid.push(bottom_row);
-    }
-    dbg!(pyramid);
+    
 }
